670fd061f00e340eb6a412be8fd2160ddd80053e
otabek <aligandi.12@gmail.com>
2022-06-01 22:45:22 +0500

    completed models and added postgresql to setting

--- HealthOnline/models.py
+++ HealthOnline/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models
 
+
 class Department(models.Model):
     title = models.CharField(max_length=100)
 
@@ -12,35 +13,48 @@ class Specialization(models.Model):
 
 
 class Room(models.Model):
-    room_number = models.IntegerField(max_length=3)
-    room_floor = models.IntegerField(max_length=2)
+    number = models.PositiveIntegerField()
+    floor = models.PositiveIntegerField(blank=True, null=True)
 
 
 class Doctor(models.Model):
-    gender = [('M', 'Male'), ('F', 'Female')]
-    first_name = models.CharField(max_length=50)
+    GENDER = [
+        ('M', 'Male'),
+        ('F', 'Female')
+    ]
+
+    id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    first_name = models.CharField(max_length=50, default='')
     last_name = models.CharField(max_length=50)
     birth_date = models.DateField()
-    gender = models.CharField(choices=gender)
+    gender = models.CharField(choices=GENDER, max_length=1)
     specialization = models.ForeignKey(Specialization, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=15)
 
 
 class Patient(models.Model):
-    gender = [('M', 'Male'), ('F', 'Female')]
+    GENDER = [
+        ('M', 'Male'),
+        ('F', 'Female')
+    ]
+
+    id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     birth_date = models.DateField()
-    gender = models.CharField(choices=gender)
+    gender = models.CharField(choices=GENDER, max_length=1)
     address = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=15)
 
 
 class PaymentType(models.Model):
-    types = [('Cash', 'Cash'), ('Online', 'Online')]
+    TYPES = [
+        ('Cash', 'Cash'),
+        ('Online', 'Online')
+    ]
 
-    title = models.CharField(choices=types)
+    title = models.CharField(choices=TYPES, max_length=6)
 
 
 class Appointment(models.Model):
@@ -58,7 +72,7 @@ class Visits(models.Model):
 
 class Payment(models.Model):
     visit = models.ForeignKey(Visits, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10)
+    amount = models.DecimalField(max_digits=10, decimal_places=4)
     payment_type = models.ForeignKey(PaymentType, on_delete=models.CASCADE)
     description = models.TextField(max_length=500)
     is_paid = models.BooleanField(default=False)
