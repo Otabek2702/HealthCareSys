8be21a4faab949b477d45295ad985c78fe769c45
otabek <aligandi.12@gmail.com>
2022-06-22 13:53:43 +0500

    register done

--- HealthOnline/views.py
+++ HealthOnline/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import DetailView
 from django.contrib.auth import authenticate, login
+# from django.contrib.auth.decorators import
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
@@ -79,20 +80,20 @@ class ServiceView(View):
 
 
 def login_user(request):
+    if request.user.is_authenticated:
+        print(request.user)
+        return redirect('profile')
     print(request.method)
     if request.method == "POST":
-        if 'username' in request.POST:
-            username = request.POST['username']
-            password = request.POST['password']
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('profile')
-            else:
-                messages.success(request, "Bunday foydalanauvchi topilmadi. Qaytadan urinib ko'ring!")
-                return redirect('login')
-        elif 'newusername' in request.POST:
-            pass 
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profile')
+        else:
+            messages.success(request, "Bunday foydalanauvchi topilmadi. Qaytadan urinib ko'ring!")
+            return redirect('login')
     else:
         return render(request, 'onlineView/signin.html')
 
@@ -102,11 +103,11 @@ def register(request):
         username = request.POST['username']
         email = request.POST['email']
         password1 = request.POST['password']
-        password2 = request.POST['password2']
 
         myuser = User.objects.create_user(username, email, password1)
         myuser.is_active = False
         myuser.save()
+        return redirect('login')
 
     return render(request, 'onlineView/register.html')
 
